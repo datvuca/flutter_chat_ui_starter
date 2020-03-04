11eb98d24e0470b94ca081e90e60c34511dff2d8
Dat Vu <13079681+datvuca@users.noreply.github.com>
2020-03-03 23:46:48 -0800

    home screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/user_model.dart';
+
+class ChatScreen extends StatefulWidget {
+  
+  final User user;
+
+  ChatScreen({this.user});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/widgets/favorite_contacts.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../models/message_model.dart';
+
+class FavoriteContacts extends StatelessWidget {
+  const FavoriteContacts({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.more_horiz),
+                  iconSize: 30.0,
+                  color: Colors.blueGrey,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 120.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.only(left: 10.0),
+              itemCount: favorites.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
+                      ),
+                      SizedBox(height: 6.0),
+                      Text(
+                        favorites[index].name,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
